0069152f629bff0f9c54e30546ce0378abced98c
Julian <julian.moik@googlemail.com>
2025-03-27 11:04:53 +0100

    add simple varcost to OP_EQUAL

--- src/script/interpreter.cpp
+++ src/script/interpreter.cpp
@@ -1051,6 +1051,7 @@ bool EvalScript(std::vector<std::vector<unsigned char> >& stack, const CScript& 
                         return set_error(serror, SCRIPT_ERR_INVALID_STACK_OPERATION);
                     valtype& vch1 = stacktop(-2);
                     valtype& vch2 = stacktop(-1);
+                    varcost += vch1.size();
                     bool fEqual = (vch1 == vch2);
                     // OP_NOTEQUAL is disabled because it would be too easy to say
                     // something like n != 1 and have some wiseguy pass in 1 with extra
