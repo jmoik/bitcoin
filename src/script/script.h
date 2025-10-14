26e54d532052ab118eb160648979427148bd7690
Julian <julian.moik@googlemail.com>
2025-10-14 15:57:37 +0200

    script: add sigversion argument to IsOpSuccess for tapscript_v2 support, extend enum class SigVersion by TAPSCRIPT_V2 and move it from interpreter.h to script.h

--- src/script/script.h
+++ src/script/script.h
@@ -240,6 +240,14 @@ static const unsigned int MAX_OPCODE = OP_NOP10;
 
 std::string GetOpName(opcodetype opcode);
 
+enum class SigVersion
+{
+    BASE = 0,        //!< Bare scripts and BIP16 P2SH-wrapped redeemscripts
+    WITNESS_V0 = 1,  //!< Witness v0 (P2WPKH and P2WSH); see BIP 141
+    TAPROOT = 2,     //!< Witness v1 with 32-byte program, not BIP16 P2SH-wrapped, key path spending; see BIP 341
+    TAPSCRIPT = 3,   //!< Witness v1 with 32-byte program, not BIP16 P2SH-wrapped, script path spending, leaf version 0xc0; see BIP 342
+    TAPSCRIPT_V2 = 4,   //!< Witness v1 with 32-byte program, not BIP16 P2SH-wrapped, script path spending, leaf version 0xc2;
+};
 class scriptnum_error : public std::runtime_error
 {
 public:
@@ -631,7 +639,7 @@ public:
 };
 
 /** Test for OP_SUCCESSx opcodes as defined by BIP342. */
-bool IsOpSuccess(const opcodetype& opcode);
+bool IsOpSuccess(const opcodetype& opcode, SigVersion sigversion = SigVersion::BASE);
 
 bool CheckMinimalPush(const std::vector<unsigned char>& data, opcodetype opcode);
 
