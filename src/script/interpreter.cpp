e4321938d11ec41477f602af2f96106c9095227e
Julian <julian.moik@googlemail.com>
2025-10-14 17:24:14 +0200

    script/interpreter: interpret final onstack value as unsigned in tapscript_v2

--- src/script/interpreter.cpp
+++ src/script/interpreter.cpp
@@ -2171,7 +2171,16 @@ static bool ExecuteWitnessScript(const Span<const valtype>& stack_span, const CS
 
     // Scripts inside witness implicitly require cleanstack behaviour
     if (stack.size() != 1) return set_error(serror, SCRIPT_ERR_CLEANSTACK);
-    if (!CastToBool(stack.back())) return set_error(serror, SCRIPT_ERR_EVAL_FALSE);
+    bool result;
+    if (sigversion == SigVersion::TAPSCRIPT_V2) {
+        size_t varcost = 0;
+        result = Val64(stack.back()).to_u64_ceil(1, varcost);
+    } else {
+        result = CastToBool(stack.back());
+    }
+    if (!result) {
+        return set_error(serror, SCRIPT_ERR_EVAL_FALSE);
+    }
     return true;
 }
 
