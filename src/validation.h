64d36ea1ab2ebeca60e4cec93614d9f13effe50e
Julian <julian.moik@googlemail.com>
2025-10-14 16:36:31 +0200

    validation: add varops_budget to CScriptCheck

--- src/validation.h
+++ src/validation.h
@@ -338,10 +338,13 @@ private:
     bool cacheStore;
     PrecomputedTransactionData *txdata;
     SignatureCache* m_signature_cache;
+    std::optional<uint64_t> varops_budget;
 
 public:
     CScriptCheck(const CTxOut& outIn, const CTransaction& txToIn, SignatureCache& signature_cache, unsigned int nInIn, script_verify_flags flags, bool cacheIn, PrecomputedTransactionData* txdataIn) :
-        m_tx_out(outIn), ptxTo(&txToIn), nIn(nInIn), m_flags(flags), cacheStore(cacheIn), txdata(txdataIn), m_signature_cache(&signature_cache) { }
+        m_tx_out(outIn), ptxTo(&txToIn), nIn(nInIn), m_flags(flags), cacheStore(cacheIn), txdata(txdataIn), m_signature_cache(&signature_cache) { 
+            varops_budget = GetTransactionWeight(*ptxTo) * VAROPS_BUDGET_PER_BYTE;
+        }
 
     CScriptCheck(const CScriptCheck&) = delete;
     CScriptCheck& operator=(const CScriptCheck&) = delete;
