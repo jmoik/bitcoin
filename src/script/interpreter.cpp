07d00cbe2fba48eb2195e4d91b09e058f17c42d1
Julian <julian.moik@googlemail.com>
2025-10-17 12:25:16 +0200

    script/interpreter: add varcost to OP_EQUAL, OP_EQUALVERIFY, OP_CHECKSIG, OP_CHECKSIGVERIFY

--- src/script/interpreter.cpp
+++ src/script/interpreter.cpp
@@ -1788,6 +1788,9 @@ bool EvalScript(ValtypeStack& stack, const CScript& script, script_verify_flags 
                     // zero bytes after it (numerically, 0x01 == 0x0001 == 0x000001)
                     //if (opcode == OP_NOTEQUAL)
                     //    fEqual = !fEqual;
+                    if (vch1.size() == vch2.size()) {
+                        varcost += vch1.size();
+                    }
                     popstack(stack);
                     popstack(stack);
                     stack.push_back(fEqual ? vchTrue : vchFalse);
@@ -2010,6 +2013,11 @@ bool EvalScript(ValtypeStack& stack, const CScript& script, script_verify_flags 
 
                     bool fSuccess = true;
                     if (!EvalChecksig(vchSig, vchPubKey, pbegincodehash, pend, execdata, flags, checker, sigversion, serror, fSuccess)) return false;
+                                        
+                    if (fSuccess) {
+                        varcost += VAROPS_COST_PER_SIGOP;
+                    }
+                    
                     popstack(stack);
                     popstack(stack);
                     stack.push_back(fSuccess ? vchTrue : vchFalse);
