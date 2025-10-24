961b3af8f48e3ab41b108f45653427da4afff5a8
Julian <julian.moik@googlemail.com>
2025-10-24 09:58:46 +0200

    policy: add MAX_STANDARD_TAPSCRIPT_V2_STACK_ITEM_SIZE

--- src/policy/policy.h
+++ src/policy/policy.h
@@ -55,6 +55,7 @@ static constexpr unsigned int MAX_STANDARD_P2WSH_STACK_ITEMS{100};
 static constexpr unsigned int MAX_STANDARD_P2WSH_STACK_ITEM_SIZE{80};
 /** The maximum size in bytes of each witness stack item in a standard BIP 342 script (Taproot, leaf version 0xc0) */
 static constexpr unsigned int MAX_STANDARD_TAPSCRIPT_STACK_ITEM_SIZE{80};
+static constexpr unsigned int MAX_STANDARD_TAPSCRIPT_V2_STACK_ITEM_SIZE{4000000}; // 4MB
 /** The maximum size in bytes of a standard witnessScript */
 static constexpr unsigned int MAX_STANDARD_P2WSH_SCRIPT_SIZE{3600};
 /** The maximum size of a standard ScriptSig */
