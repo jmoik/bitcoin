0ac92f66145b621d3ee26b91da0c9f857a5b5c80
Julian <julian.moik@googlemail.com>
2026-02-01 16:48:10 +0100

    script/interpreter: tapscript v2: improve OP_TUCK performance with swap instead of insert

--- src/script/interpreter.cpp
+++ src/script/interpreter.cpp
@@ -1730,7 +1730,9 @@ bool EvalScript(ValtypeStack& stack, const CScript& script, script_verify_flags 
                     // |OP_TUCK
                     // |Length of second-from-top stack entry (before) (COPYING)
                     varcost += vch.size();
-                    stack.insert(stack.size() - 2, vch);
+                    // (x1 x2) -> push x2 -> (x1 x2 x2) -> swap(-2,-3) -> (x2 x1 x2)
+                    stack.push_back(vch);
+                    stack.swap(-2, -3);
                 }
                 break;
 
